911203cb42ed373f9b4f255907bdcdd176dd98e1
Abe <abealto9@gmail.com>
2022-09-03 16:05:24 -0400

    updated guessinggame component

--- src/GuessingGame.js
+++ src/GuessingGame.js
@@ -1,11 +1,22 @@
 import react from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 function GuessingGame() {
   const gameText =
     'I am thinking of a number between 1 and 100. Guess the Lucky Number!';
+  const numOfGuess = 'you have made ... guesses';
+  const currect = 'You guessed currectly';
+  const tooLow = 'too low';
+  const tooHight = 'too high';
   return (
     <div>
       <p>{gameText}</p>
+      <p>{numOfGuess}</p>
+      <Form>
+        <Form.Group className='mb-3'></Form.Group>
+        <Button type='submit'>Guess</Button>
+      </Form>
     </div>
   );
 }
