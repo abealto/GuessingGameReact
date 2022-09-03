129bbcb775163a05e56b81298e09bd6f745fafb9
Abe <abealto9@gmail.com>
2022-09-03 15:43:49 -0400

    first commit

---
+++ src/GuessingGame.js
@@ -0,0 +1,12 @@
+import react from 'react';
+
+function GuessingGame() {
+  const gameText =
+    'I am thinking of a number between 1 and 100. Guess the Lucky Number!';
+  return (
+    <div>
+      <p>{gameText}</p>
+    </div>
+  );
+}
+export default GuessingGame;
