b2fddfef78df8e06849db8c2ec8819175912312a
Abe <abealto9@gmail.com>
2022-09-04 06:53:49 -0400

    saving user guess to state

--- src/GuessingGame.js
+++ src/GuessingGame.js
@@ -1,20 +1,29 @@
-import react from 'react';
+import { react, useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
 
 function GuessingGame() {
+  const [guess, setGuess] = useState();
+
   const gameText =
     'I am thinking of a number between 1 and 100. Guess the Lucky Number!';
   const numOfGuess = 'you have made ... guesses';
   const currect = 'You guessed currectly';
   const tooLow = 'too low';
   const tooHight = 'too high';
+
+  function handleChange(e) {
+    setGuess(e.target.value);
+  }
+
   return (
     <div>
       <p>{gameText}</p>
       <p>{numOfGuess}</p>
       <Form>
         <Form.Group className='mb-3'></Form.Group>
+        <Form.Control type='text' onChange={handleChange} name='userGuess' />
         <Button type='submit'>Guess</Button>
       </Form>
     </div>
